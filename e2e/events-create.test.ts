b40c55500b40d7080bdc7e97864c970dd628313d
stefdelec <stephane@arianee.org>
2020-06-03 21:46:59 +0200

    test(e2e): add test on message, certificate, event

--- e2e/events-create.test.ts
+++ e2e/events-create.test.ts
@@ -3,17 +3,20 @@ import {ArianeeWalletBuilder} from "@arianee/arianeejs/dist/src/core/wallet/wall
 import {ArianeeWallet} from "@arianee/arianeejs/dist/src/core/wallet";
 import {certificateContent} from "./mocks/certificateContent";
 import {cloneDeep} from 'lodash';
+import {eventContent} from "./mocks/eventContent";
 
-describe('Certificate', () => {
+describe('Event', () => {
+    const arianeeEventId = 4254908;
     const certificateId = 7371300;
     let arianee: ArianeeWalletBuilder;
     let wallet: ArianeeWallet;
+
     beforeAll(async () => {
         arianee = await new Arianee().init(NETWORK.testnet);
         wallet = arianee.fromPrivateKey(process.env.privateKey)
     });
     test('should be able create content if content is equal to imprint', async (done) => {
-        await wallet.methods.storeContentInRPCServer(certificateId, certificateContent, 'http://localhost:3000/rpc');
+        await wallet.methods.storeArianeeEvent(certificateId, arianeeEventId, eventContent, 'http://localhost:3000/rpc');
         expect(true).toBeTruthy();
         done()
     });
@@ -21,12 +24,12 @@ describe('Certificate', () => {
 
     test('should NOT be able create content if content is not equal to imprint', async (done) => {
         let isInError = false;
-        const certificateClone = cloneDeep(certificateContent);
-        certificateClone.name = 'anotherName';
+        const eventContentClone = cloneDeep(eventContent);
+        eventContentClone.title = 'anotherTitle';
 
         try {
+            await wallet.methods.storeArianeeEvent(certificateId, arianeeEventId, eventContentClone, 'http://localhost:3000/rpc');
 
-            await wallet.methods.storeContentInRPCServer(certificateId, certificateClone, 'http://localhost:3000/rpc');
         } catch (e) {
             isInError = true;
         }
@@ -37,20 +40,15 @@ describe('Certificate', () => {
     });
 
     test('should be able get content', async (done) => {
-        await wallet.methods.storeContentInRPCServer(certificateId, certificateContent, 'http://localhost:3000/rpc');
+        await wallet.methods.storeArianeeEvent(certificateId, arianeeEventId, eventContent, 'http://localhost:3000/rpc');
+
         const result = await wallet.methods.getCertificate(certificateId, undefined,
-            {content: true, issuer: {rpcURI: 'http://localhost:3000/rpc'}});
+            {arianeeEvents: true, issuer: {rpcURI: 'http://localhost:3000/rpc'}});
 
+        const arianeeEvent = result.events.arianeeEvents.find(d => d.arianeeEventId.toString() === arianeeEventId.toString());
 
-        expect(result.content.data).toEqual(certificateContent);
+        expect(arianeeEvent.content.data).toEqual(eventContent);
         done()
 
     })
 });
-
-
-
-
-
-
-
