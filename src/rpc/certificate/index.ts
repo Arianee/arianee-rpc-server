10d30012d7b3efa4047a0055a81aa94473d4e3ab
Maxime <maxime@vaugam.com>
2020-06-22 11:15:54 +0200

    feat(jwt): add jwt as authentication method

--- src/rpc/certificate/index.ts
+++ src/rpc/certificate/index.ts
@@ -64,7 +64,7 @@ const certificateRPCFactory = (fetchItem,createItem, network) => {
    * @param data
    * @param callback
    */
-  const read = async (data:CertificatePayload, callback:SyncFunc) => {
+  const read = async (data: CertificatePayload, callback: SyncFunc) => {
     const successCallBack = async () => {
       try {
         const content = await fetchItem(certificateId);
@@ -77,48 +77,59 @@ const certificateRPCFactory = (fetchItem,createItem, network) => {
     const arianee = await new Arianee().init(network);
     const tempWallet = arianee.fromRandomKey();
 
-    const { certificateId, authentification } = data;
-    const { message, signature } = authentification;
+    const {certificateId, authentification} = data;
+    const {message, signature, jwt} = authentification;
 
-    const publicAddressOfSender = tempWallet.web3.eth.accounts.recover(
-      message,
-      signature
-    );
-
-    const parsedMessage = JSON.parse(message);
-
-    if (parsedMessage.certificateId !== certificateId) {
-      return callback(getError(ErrorEnum.WRONGCERTIFICATEID));
-    }
-
-    const isSignatureTooOld =
-      (new Date().getTime() - new Date(message.timestamp).getTime()) / 1000 >
-      300;
-
-    if (isSignatureTooOld) {
-      return callback(getError(ErrorEnum.SIGNATURETOOOLD));
-    }
-
-    // Is user the owner of this certificate
-    const owner = await tempWallet.contracts.smartAssetContract.methods
-      .ownerOf(certificateId)
-      .call();
-
-    if (owner === publicAddressOfSender) {
-      return successCallBack();
-    }
-
-    // Is the user provide a token acces
-    for (let tokenType = 0; tokenType < 4; tokenType++) {
-      const data = await tempWallet.contracts.smartAssetContract.methods
-        .tokenHashedAccess(certificateId, tokenType)
-        .call();
-
-      if (publicAddressOfSender === data) {
+    if (jwt) {
+      const isJWTValid = await tempWallet.methods.isJWTProofValid(jwt);
+      const {payload} = await tempWallet.methods.decodeJWTProof(jwt);
+      if (isJWTValid && (payload.subId === certificateId)) {
         return successCallBack();
+      } else {
+        return callback(getError(ErrorEnum.WRONGJWT));
       }
     }
 
+    if (message && signature) {
+      const publicAddressOfSender = tempWallet.web3.eth.accounts.recover(
+        message,
+        signature
+      );
+
+      const parsedMessage = JSON.parse(message);
+
+      if (parsedMessage.certificateId !== certificateId) {
+        return callback(getError(ErrorEnum.WRONGCERTIFICATEID));
+      }
+
+      const isSignatureTooOld =
+        (new Date().getTime() - new Date(message.timestamp).getTime()) / 1000 >
+        300;
+
+      if (isSignatureTooOld) {
+        return callback(getError(ErrorEnum.SIGNATURETOOOLD));
+      }
+
+      // Is user the owner of this certificate
+      const owner = await tempWallet.contracts.smartAssetContract.methods
+        .ownerOf(certificateId)
+        .call();
+
+      if (owner === publicAddressOfSender) {
+        return successCallBack();
+      }
+
+      // Is the user provide a token acces
+      for (let tokenType = 0; tokenType < 4; tokenType++) {
+        const data = await tempWallet.contracts.smartAssetContract.methods
+          .tokenHashedAccess(certificateId, tokenType)
+          .call();
+
+        if (publicAddressOfSender === data) {
+          return successCallBack();
+        }
+      }
+    }
     return callback(getError(ErrorEnum.MAINERROR));
   };
 
--- src/rpc/errors/error.ts
+++ src/rpc/errors/error.ts
@@ -7,7 +7,8 @@ export const enum ErrorEnum {
     SIGNATURETOOOLD='SIGNATURETOOOLD',
     WRONGIMPRINT='WRONGIMPRINT',
     JSONSCHEMA='JSONSCHEMA',
-    WRONGMESSAGEID='WRONGMESSAGEID'
+    WRONGMESSAGEID='WRONGMESSAGEID',
+    WRONGJWT = "WRONGJWT"
 
 
 }
@@ -19,6 +20,7 @@ const errorLog={
    [ErrorEnum.WRONGIMPRINT]  : { code: 4, message: "Imprint does not match content" },
    [ErrorEnum.JSONSCHEMA]  : { code: 5, message: "JSON Schema is unreachable" },
    [ErrorEnum.WRONGMESSAGEID]  : { code: 6, message: "messageId in header and data are not the same " },
+    [ErrorEnum.WRONGJWT]  : { code: 7, message: "the JWT is not valid" },
 
 }
 
